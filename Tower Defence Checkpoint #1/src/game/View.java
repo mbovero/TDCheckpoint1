87a9abc9e782ccb5b21d721873c5dae09987a261
mbovero <milesbovero@icloud.com>
2022-11-11 13:28:30 -0700

    Finished step 9 of checkpoint 1

--- Tower Defence Checkpoint #1/src/game/View.java
+++ Tower Defence Checkpoint #1/src/game/View.java
@@ -1,5 +1,10 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class View extends JPanel {
@@ -11,9 +16,25 @@ public class View extends JPanel {
 	{
 		this.control = control;
 		this.state = state;
+		
+        JFrame f = new JFrame("Miles's & Kirt's Tower Defense 2022");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        f.setContentPane(this);
+        this.setMinimumSize(new Dimension(600, 600));
+        this.setPreferredSize(this.getMinimumSize());
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
 	}
 	
-	public static void main(String[] args) {
+	public void paint(Graphics g)
+	{
+		g.setColor(Color.blue);
+		g.fillRect(150, 150, 150, 150);
+	}
+	
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
